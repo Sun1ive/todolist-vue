ef393939587887f78cfbdc90b7d4b102a115381a
Sun1ive <sunliveua@gmail.com>
2019-08-13 23:48:26 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import { plugin } from 'vue-function-api';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { createProvider } from './vue-apollo';
+
+Vue.config.productionTip = false;
+
+Vue.use(plugin);
+
+new Vue({
+  router,
+  store,
+  apolloProvider: createProvider(),
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    hello
+    <button @click="submit">Register</button>
+    <input placeholder="username" type="text" v-model="username" />
+    <input placeholder="email" type="text" v-model="email" />
+    <input placeholder="password" type="text" v-model="password" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import gql from 'graphql-tag';
+
+export const REGISTRATION_MUTATION = gql`
+  mutation Register($email: String!, $username: String, $password: String!) {
+    Register(
+      data: { username: $username, password: $password, email: $email }
+    ) {
+      id
+      email
+      username
+      token
+    }
+  }
+`;
+
+export default {
+  name: 'home',
+
+  data: () => ({
+    email: '',
+    password: '',
+    username: '',
+  }),
+
+  methods: {
+    async submit() {
+      try {
+        const r = await this.$apollo.mutate({
+          mutation: REGISTRATION_MUTATION,
+          variables: {
+            email: this.email,
+            password: this.password,
+            username: this.username,
+          },
+        });
+        console.log(r);
+      } catch (error) {
+        console.log('error', JSON.stringify(error));
+      }
+    },
+  },
+};
+</script>
